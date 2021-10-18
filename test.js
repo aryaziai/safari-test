2217810899a347753b9cd275d12e5a897a758b0b
aryaziai <54697831+aryaziai@users.noreply.github.com>
2021-10-18 16:28:22 -0700

    Create test.js

---
+++ test.js
@@ -0,0 +1,30 @@
+function addStyleString(str) {
+    var node = document.createElement('style');
+    node.innerHTML = str;
+    document.body.appendChild(node);
+}
+
+site = window.location.hostname.split('.').slice(-2).join('.').split(".com")[0]
+
+// Twitter
+if (site == "twitter") {
+document.querySelector('[role="banner"]').firstElementChild.remove();
+document.querySelector('[role="banner"]').firstElementChild.style.cssText ="position:relative;overflow:hidden"
+
+document.querySelector('a[href="/explore"]').remove();
+
+addStyleString('div[data-testid="placementTracking"]{ display: none !important }');
+
+}
+
+// Instagram
+if (site == "instagram") {
+document.querySelector("nav").firstElementChild.firstElementChild.firstElementChild.style = "position: relative"
+
+document.querySelector('a[href="/explore/"]').parentElement.remove();
+
+document.querySelector('div[data-testid="new-post-button"]').remove();
+};
+
+// Call completion to finish
+completion();
