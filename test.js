c92f3b87b365e57729fb3eecac7a074dd218b015
aryaziai <54697831+aryaziai@users.noreply.github.com>
2021-10-19 00:25:59 -0700

    Update test.js

--- test.js
+++ test.js
@@ -17,7 +17,7 @@ if (site == "instagram") {
     resolve();
   }).then(() => {
     document.querySelector("nav").firstElementChild.firstElementChild.firstElementChild.style = "position: relative";
-    document.querySelector("section > main").remove();
+    document.querySelector(".o64aR").remove();
     document.querySelector('div[data-testid="new-post-button"]').remove();
   });
 };
