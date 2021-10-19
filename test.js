d9720f036d8d2e8e433c2c60907b93026575b141
aryaziai <54697831+aryaziai@users.noreply.github.com>
2021-10-18 19:55:18 -0700

    Update test.js

--- test.js
+++ test.js
@@ -1,6 +1,7 @@
 site = window.location.hostname.split(".").slice(-2).join(".").split(".com")[0];
 
 // Twitter
+if (site == "instagram") {
 const myDisplay = new Promise((resolve, reject) => {
   resolve();
 }).then(() => {
@@ -8,6 +9,7 @@ const myDisplay = new Promise((resolve, reject) => {
   document.querySelector('[role="banner"]').firstElementChild.remove();
   document.querySelector('[role="banner"]').firstElementChild.style.cssText = "position:relative;overflow:hidden";
 });
+};
 
 // Instagram
 if (site == "instagram") {
@@ -18,4 +20,4 @@ if (site == "instagram") {
     document.querySelector('a[href="/explore/"]').parentElement.remove();
     document.querySelector('div[data-testid="new-post-button"]').remove();
   });
-}
+};
