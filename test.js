d331fa7803b20ef52d347a317c811fa18a778e1b
aryaziai <54697831+aryaziai@users.noreply.github.com>
2021-10-18 19:29:52 -0700

    Update test.js

--- test.js
+++ test.js
@@ -8,8 +8,6 @@ site = window.location.hostname.split('.').slice(-2).join('.').split(".com")[0]
 document.querySelector('[role="banner"]').firstElementChild.remove();
 document.querySelector('[role="banner"]').firstElementChild.style.cssText ="position:relative;overflow:hidden"
 
-document.querySelector('a[href="/explore"]').remove();
-
 
       
 // Instagram
@@ -20,17 +18,7 @@ if (site == "instagram") {
 })
   .then(() => {
 document.querySelector("nav").firstElementChild.firstElementChild.firstElementChild.style = "position: relative"
-
 document.querySelector('a[href="/explore/"]').parentElement.remove();
-
 document.querySelector('div[data-testid="new-post-button"]').remove();
   });
-   
-
-    
-    
-//     window.alert("IG");
-    
-
 };
-
