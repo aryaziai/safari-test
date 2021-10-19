ba5a514f71e5f734b26366d17bad85e59952227c
aryaziai <54697831+aryaziai@users.noreply.github.com>
2021-10-18 19:28:06 -0700

    Update test.js

--- test.js
+++ test.js
@@ -1,32 +1,18 @@
-
-
-function addStyleString(str) {
-    var node = document.createElement('style');
-    node.innerHTML = str;
-    document.body.appendChild(node);
-}
-
 site = window.location.hostname.split('.').slice(-2).join('.').split(".com")[0]
 
 // Twitter
-// if (site == "twitter") {
-//     window.alert("Twitter");
-// document.querySelector('[role="banner"]').firstElementChild.remove();
-// document.querySelector('[role="banner"]').firstElementChild.style.cssText ="position:relative;overflow:hidden"
+ const myDisplay = new Promise((resolve, reject) => {
+  resolve();
+})
+  .then(() => {
+document.querySelector('[role="banner"]').firstElementChild.remove();
+document.querySelector('[role="banner"]').firstElementChild.style.cssText ="position:relative;overflow:hidden"
 
-// document.querySelector('a[href="/explore"]').remove();
-
-// addStyleString('div[data-testid="placementTracking"]{ display: none !important }');
-
-// }
+document.querySelector('a[href="/explore"]').remove();
 
 
-
+      
 // Instagram
-
-
-
-
 if (site == "instagram") {
     
  const myDisplay = new Promise((resolve, reject) => {
