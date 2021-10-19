af17489e8ec1de11444139fbf17e15bdcadd7e72
aryaziai <54697831+aryaziai@users.noreply.github.com>
2021-10-18 19:31:28 -0700

    Update test.js

--- test.js
+++ test.js
@@ -1,24 +1,20 @@
-site = window.location.hostname.split('.').slice(-2).join('.').split(".com")[0]
+site = window.location.hostname.split(".").slice(-2).join(".").split(".com")[0];
 
 // Twitter
- const myDisplay = new Promise((resolve, reject) => {
+const myDisplay = new Promise((resolve, reject) => {
   resolve();
-})
-  .then(() => {
-document.querySelector('[role="banner"]').firstElementChild.remove();
-document.querySelector('[role="banner"]').firstElementChild.style.cssText ="position:relative;overflow:hidden"
+}).then(() => {
+  document.querySelector('[role="banner"]').firstElementChild.remove();
+  document.querySelector('[role="banner"]').firstElementChild.style.cssText = "position:relative;overflow:hidden";
+});
 
-
-      
 // Instagram
 if (site == "instagram") {
-    
- const myDisplay = new Promise((resolve, reject) => {
-  resolve();
-})
-  .then(() => {
-document.querySelector("nav").firstElementChild.firstElementChild.firstElementChild.style = "position: relative"
-document.querySelector('a[href="/explore/"]').parentElement.remove();
-document.querySelector('div[data-testid="new-post-button"]').remove();
+  const myDisplay = new Promise((resolve, reject) => {
+    resolve();
+  }).then(() => {
+    document.querySelector("nav").firstElementChild.firstElementChild.firstElementChild.style = "position: relative";
+    document.querySelector('a[href="/explore/"]').parentElement.remove();
+    document.querySelector('div[data-testid="new-post-button"]').remove();
   });
-};
+}
