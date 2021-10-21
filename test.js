a02c9311ac584418d5f5341df2eac4cbea313aec
aryaziai <arya721@gmail.com>
2021-10-21 11:57:24 -0700

    test

--- test.js
+++ test.js
@@ -1,5 +1,16 @@
 site = window.location.hostname.split(".").slice(-2).join(".").split(".com")[0];
 
+if (site == "instagram") {
+  const myDisplay = new Promise((resolve, reject) => {
+    resolve();
+  }).then(() => {
+    document.querySelector('a[href="/explore/"]').parentElement.style.cssText = "display:none";
+    document.querySelector("nav").firstElementChild.firstElementChild.firstElementChild.style = "position: relative";
+    document.querySelector('div[data-testid="mobile-nav-logged-in"] > div:nth-child(2)').style.cssText = "display:none";
+    document.querySelector('div[data-testid="new-post-button"]').style.cssText = "display:none";
+  });
+}
+
 if (site == "twitter") {
   const myDisplay = new Promise((resolve, reject) => {
     resolve();
@@ -10,22 +21,10 @@ if (site == "twitter") {
   });
 }
 
-if (site == "instagram") {
-  const myDisplay = new Promise((resolve, reject) => {
-    resolve();
-  }).then(() => {
-		document.querySelector('a[href="/explore/"]').parentElement.style.cssText = "display:none";
-    document.querySelector("nav").firstElementChild.firstElementChild.firstElementChild.style = "position: relative";
-    document.querySelector('div[data-testid="mobile-nav-logged-in"] > div:nth-child(2)').style.cssText = "display:none";
-    document.querySelector('div[data-testid="new-post-button"]').style.cssText = "display:none";
-  });
-}
-
 if (site == "reddit") {
   const myDisplay = new Promise((resolve, reject) => {
     resolve();
   }).then(() => {
-
     document.querySelectorAll('[rel="nofollow noopener sponsored"]').forEach((r) => r.parentElement.remove());
   });
 }
