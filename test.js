5a2b64d0f5fceae6ab330d29430db4ed25af1fb0
aryaziai <arya721@gmail.com>
2021-10-20 21:25:50 -0700

    .

--- test.js
+++ test.js
@@ -1,29 +1,29 @@
-site = window.location.hostname.split(".").slice(-2).join(".").split(".com")[0];
+// site = window.location.hostname.split(".").slice(-2).join(".").split(".com")[0];
 
-if (site == "twitter") {
-  const myDisplay = new Promise((resolve, reject) => {
-    resolve();
-  }).then(() => {
-    document.querySelector('header[role="banner"]').children[0].style.cssText = "display:none";
-    document.querySelector('header[role="banner"]').children[1].style.cssText = "position:relative;overflow:hidden";
-    document.querySelector('a[href="/explore"]').style.cssText = "display:none";
-  });
-}
+// if (site == "twitter") {
+//   const myDisplay = new Promise((resolve, reject) => {
+//     resolve();
+//   }).then(() => {
+//     document.querySelector('header[role="banner"]').children[0].style.cssText = "display:none";
+//     document.querySelector('header[role="banner"]').children[1].style.cssText = "position:relative;overflow:hidden";
+//     document.querySelector('a[href="/explore"]').style.cssText = "display:none";
+//   });
+// }
 
-if (site == "instagram") {
-  const myDisplay = new Promise((resolve, reject) => {
-    resolve();
-  }).then(() => {
-    document.querySelector("nav").firstElementChild.firstElementChild.firstElementChild.style = "position: relative";
-    document.querySelector('a[href="/explore/"]').parentElement.style.cssText = "display:none";
-    document.querySelector('div[data-testid="new-post-button"]').style.cssText = "display:none";
-  });
-}
+// if (site == "instagram") {
+//   const myDisplay = new Promise((resolve, reject) => {
+//     resolve();
+//   }).then(() => {
+//     document.querySelector("nav").firstElementChild.firstElementChild.firstElementChild.style = "position: relative";
+//     document.querySelector('a[href="/explore/"]').parentElement.style.cssText = "display:none";
+//     document.querySelector('div[data-testid="new-post-button"]').style.cssText = "display:none";
+//   });
+// }
 
-if (site == "reddit") {
-  const myDisplay = new Promise((resolve, reject) => {
-    resolve();
-  }).then(() => {
-    document.querySelectorAll('[rel="nofollow noopener sponsored"]').forEach((r) => r.parentElement.remove());
-  });
-}
+// if (site == "reddit") {
+//   const myDisplay = new Promise((resolve, reject) => {
+//     resolve();
+//   }).then(() => {
+//     document.querySelectorAll('[rel="nofollow noopener sponsored"]').forEach((r) => r.parentElement.remove());
+//   });
+// }
